6646c8fd7d8a0bad7d74aba9c322c3d19cb43e9d
eoaooeoa <eoaooeoa@gmail.com>
2022-11-01 16:58:18 +0900

    add product api for CRUD

---
+++ src/db/schemas/product-schema.js
@@ -0,0 +1,39 @@
+import { Schema } from "mongoose";
+
+const ProductSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    publisher: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+    },
+    description: {
+      type: String,
+    },
+    rate: {
+      type: Number,
+      default: 0
+    },
+  },
+  {
+    collection: "products",
+    timestamps: true,
+  }
+);
+
+export { ProductSchema };
