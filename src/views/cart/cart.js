fa6ddf1121ff77009c36fa456a94bab0b1d94770
odhdhfjf <odhdhfjf@gmail.com>
2022-11-03 14:23:15 +0900

    장바구니 상호작용시 바로 로컬스토리지에 저장하게 변경

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,13 +1,13 @@
 // 이 아래 3 변수는 임의로 데이터를 추가하기 위한 변수임
-const submitBtn = document.querySelector('.submit');
-const bookName = document.querySelector('.bookName');
-const bookPrice = document.querySelector('.bookPrice');
+const submitBtn = document.querySelector(".submit");
+const bookName = document.querySelector(".bookName");
+const bookPrice = document.querySelector(".bookPrice");
 
-const li = document.querySelector('.list'); // 전체 장바구니 목록을 포함하는 ul
-const selectDelBtn = document.querySelector('.selectDelBtn'); //선택 삭제 버튼
-const totalPrice = document.querySelector('.totalPrice');
-const selectAllBtn = document.querySelector('.selectAll');
-let baskets = JSON.parse(localStorage.getItem('baskets')) || [];
+const li = document.querySelector(".list"); // 전체 장바구니 목록을 포함하는 ul
+const selectDelBtn = document.querySelector(".selectDelBtn"); //선택 삭제 버튼
+const totalPrice = document.querySelector(".totalPrice");
+const selectAllBtn = document.querySelector(".selectAll");
+let baskets = JSON.parse(localStorage.getItem("carts")) || [];
 let checkList = [];
 let totalPriceValue = 0;
 
@@ -42,37 +42,55 @@ function template(img, name, price, count, idx) {
   </li>`;
 }
 
+function setItem(name, type) {
+  const index = baskets.findIndex((obj) => {
+    return obj.title == name;
+  });
+  if (type == "plus") {
+    baskets[index].count += 1;
+    localStorage.setItem("carts", JSON.stringify(baskets));
+  } else if (type == "minus") {
+    baskets[index].count -= 1;
+    localStorage.setItem("carts", JSON.stringify(baskets));
+  } else {
+    baskets[index].count = Number(type);
+
+    localStorage.setItem("carts", JSON.stringify(baskets));
+  }
+}
+
 function add(e) {
   e.preventDefault();
   const name = bookName.value;
   const price = bookPrice.value;
   let check = true;
   const basket = {
-    name: name,
+    title: name,
     price: price,
     count: baskets.count || 1,
+    imgaes: "url",
   };
 
   baskets.forEach((element, idx) => {
-    if (element.name === basket.name) {
+    if (element.title === basket.name) {
       baskets[idx].count += 1;
-      localStorage.setItem('baskets', JSON.stringify(baskets));
-      check = false;
+      localStorage.setItem("carts", JSON.stringify(baskets));
       return false;
     }
   });
 
   if (check === true) {
     baskets.push(basket);
-    localStorage.setItem('baskets', JSON.stringify(baskets));
+    localStorage.setItem("carts", JSON.stringify(baskets));
   }
 }
 
 function minusCount(box) {
-  const input = box.querySelector('#count');
+  const input = box.querySelector("#count");
   const inputVal = input.value;
-  const totalPrice1 = box.querySelector('.totalPrice');
-  const price = box.querySelector('.price');
+  const name = box.querySelector(".bookName").innerHTML;
+  const totalPrice1 = box.querySelector(".totalPrice");
+  const price = box.querySelector(".price");
 
   if (inputVal <= 1) {
     return (totalPrice1.innerHTML = price.innerHTML);
@@ -81,13 +99,15 @@ function minusCount(box) {
   totalPrice1.innerHTML = `${Number(price.innerHTML) * Number(input.value)}`;
   totalPriceValue = totalPriceValue - Number(price.innerHTML);
   totalPrice.innerHTML = totalPriceValue;
+  setItem(name, "minus");
 }
 
 function addCount(box) {
-  const input = box.querySelector('#count');
+  const input = box.querySelector("#count");
   const inputVal = input.value;
-  const totalPrice1 = box.querySelector('.totalPrice');
-  const price = box.querySelector('.price');
+  const name = box.querySelector(".bookName").innerHTML;
+  const totalPrice1 = box.querySelector(".totalPrice");
+  const price = box.querySelector(".price");
 
   if (inputVal >= 999) {
     input.value = 999;
@@ -96,19 +116,20 @@ function addCount(box) {
     totalPrice1.innerHTML = `${Number(price.innerHTML) * Number(input.value)}`;
     totalPriceValue = totalPriceValue + Number(price.innerHTML);
     totalPrice.innerHTML = totalPriceValue;
+    setItem(name, "plus");
   }
 }
 
 function inputCount(target, box) {
-  const totalPrice1 = box.querySelector('.totalPrice');
-  const price = box.querySelector('.price');
+  const totalPrice1 = box.querySelector(".totalPrice");
+  const price = box.querySelector(".price");
+  const name = box.querySelector(".bookName").innerHTML;
   target.oninput = () => {
     if (target.value.length > 3) {
       target.value = 999;
     } else if (target.value <= 1) {
       target.value = 1;
     }
-
     const a =
       Number(price.innerHTML) * Number(target.value) -
       Number(totalPrice1.innerHTML);
@@ -116,34 +137,32 @@ function inputCount(target, box) {
       totalPriceValue += a;
       totalPrice.innerHTML = totalPriceValue;
     }
-
     totalPrice1.innerHTML = `${Number(price.innerHTML) * Number(target.value)}`;
+    setItem(name, target.value);
   };
 }
 
 function clickDelBtn(box) {
-  const name = box.querySelector('.bookName').innerHTML;
-  const totalPrice1 = box.querySelector('.totalPrice');
-  console.log(typeof totalPriceValue);
-  console.log(baskets);
-  box.style.display = 'none';
+  const name = box.querySelector(".bookName").innerHTML;
+  const totalPrice1 = box.querySelector(".totalPrice");
+  box.style.display = "none";
   const index = baskets.findIndex((obj) => {
-    return obj.name == name;
+    return obj.title == name;
   });
 
   baskets.splice(index, 1);
   console.log(baskets);
-  localStorage.setItem('baskets', JSON.stringify(baskets));
+  localStorage.setItem("carts", JSON.stringify(baskets));
   totalPriceValue -= Number(totalPrice1.innerHTML);
   totalPrice.innerHTML = totalPriceValue;
 }
 
 function clickSelBtn(box) {
   console.log(box);
-  const selectBtn = box.querySelector('#select');
-  const name = box.querySelector('.bookName').innerHTML;
-  if (selectBtn.style.backgroundColor == 'blue') {
-    selectBtn.style.backgroundColor = 'white';
+  const selectBtn = box.querySelector("#select");
+  const name = box.querySelector(".bookName").innerHTML;
+  if (selectBtn.style.backgroundColor == "blue") {
+    selectBtn.style.backgroundColor = "white";
     selectBtn.style.opacity = 0.4;
 
     checkList = checkList.filter((val) => {
@@ -151,7 +170,7 @@ function clickSelBtn(box) {
       return val != name;
     });
   } else {
-    selectBtn.style.backgroundColor = 'blue';
+    selectBtn.style.backgroundColor = "blue";
     selectBtn.style.opacity = 1;
     checkList.push(name);
   }
@@ -160,33 +179,33 @@ function clickSelBtn(box) {
 function clickSelDelBtn() {
   checkList.forEach((val) => {
     const index = baskets.findIndex((obj) => {
-      return obj.name == val;
+      return obj.title == val;
     });
     baskets.splice(index, 1);
 
     console.log(baskets);
-    localStorage.setItem('baskets', JSON.stringify(baskets));
+    localStorage.setItem("carts", JSON.stringify(baskets));
   });
 }
 
 function allSelect(boxes) {
-  if (selectAllBtn.style.backgroundColor == 'blue') {
-    selectAllBtn.style.backgroundColor = 'white';
+  if (selectAllBtn.style.backgroundColor == "blue") {
+    selectAllBtn.style.backgroundColor = "white";
     selectAllBtn.style.opacity = 0.4;
     checkList = [];
     boxes.forEach((box) => {
-      const selectBtn = box.querySelector('#select');
-      selectBtn.style.backgroundColor = 'white';
+      const selectBtn = box.querySelector("#select");
+      selectBtn.style.backgroundColor = "white";
       selectBtn.style.opacity = 0.4;
     });
   } else {
-    selectAllBtn.style.backgroundColor = 'blue';
+    selectAllBtn.style.backgroundColor = "blue";
     selectAllBtn.style.opacity = 1;
     checkList = [];
     boxes.forEach((box) => {
-      const selectBtn = box.querySelector('#select');
-      const name = box.querySelector('.bookName').innerHTML;
-      selectBtn.style.backgroundColor = 'blue';
+      const selectBtn = box.querySelector("#select");
+      const name = box.querySelector(".bookName").innerHTML;
+      selectBtn.style.backgroundColor = "blue";
       selectBtn.style.opacity = 1;
       checkList.push(name);
     });
@@ -196,35 +215,35 @@ function allSelect(boxes) {
 if (baskets.length > 0) {
   baskets.forEach((basket, idx) => {
     li.insertAdjacentHTML(
-      'beforeend',
-      template('', basket.name, basket.price, basket.count, idx)
+      "beforeend",
+      template(basket.imgaes, basket.title, basket.price, basket.count, idx)
     );
     totalPriceValue += basket.price * basket.count;
   });
 }
 totalPrice.innerHTML = totalPriceValue;
-const boxes = document.querySelectorAll('.product');
+const boxes = document.querySelectorAll(".product");
 console.log(boxes);
 li.onclick = (event) => {
   console.log(totalPriceValue);
   const box = event.target.parentNode.parentNode.parentNode;
   const box2 = event.target.parentNode.parentNode;
-  if (event.target.id == 'plusBtn') {
+  if (event.target.id == "plusBtn") {
     addCount(box);
-  } else if (event.target.id == 'minusBtn') {
+  } else if (event.target.id == "minusBtn") {
     minusCount(box);
-  } else if (event.target.id == 'count') {
+  } else if (event.target.id == "count") {
     inputCount(event.target, box);
-  } else if (event.target.id == 'deleteBtn') {
+  } else if (event.target.id == "deleteBtn") {
     clickDelBtn(box2);
-  } else if (event.target.id == 'select') {
+  } else if (event.target.id == "select") {
     clickSelBtn(box2);
   }
 };
 
-selectDelBtn.addEventListener('click', clickSelDelBtn);
-submitBtn.addEventListener('click', add);
-selectAllBtn.addEventListener('click', function () {
+selectDelBtn.addEventListener("click", clickSelDelBtn);
+submitBtn.addEventListener("click", add);
+selectAllBtn.addEventListener("click", function () {
   allSelect(boxes);
   console.log(checkList);
 });
