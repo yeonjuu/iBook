a868caa71be42ff21df2f25c605d459ea1407cb5
odhdhfjf <odhdhfjf@gmail.com>
2022-11-03 17:00:32 +0900

    유저 상태 따라 화면 다르게 구현

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -2,13 +2,53 @@ const li = document.querySelector(".li");
 const productPrice = document.querySelector(".productPrice");
 const totalPrice = document.querySelector(".totalPrice");
 const delivery = document.querySelector(".deliveryPrice");
+const body = document.querySelector(".hidden");
+const orderer = document.querySelector("#orderer");
+const email = document.querySelector("#email");
+const phoneNumber = document.querySelector("#phoneNumber");
+const ordererInfoContents = document.querySelector(".ordererInfoContents");
+const submitBtn = document.querySelector(".submitBtn");
+const Info = document.querySelector(".Info");
 
+let password;
+let checkPassword;
+console.log(submitBtn);
+
+let userCheck = false;
 let deliveryPrice = 0;
 let baskets = JSON.parse(localStorage.getItem("carts")) || [];
 let totalPriceValue = 0;
 let deliveryMin = 30000;
 
-function template(img, title, price, count) {
+function passwordHtml() {
+  return `<div class="password inputSort">
+    <div><strong>주문조회 비밀번호</strong></div>
+    <div>
+      <input
+        id="password"
+        type="text"
+        placeholder="비밀번호를 입력해 주세요"
+        required
+      />
+      <p>
+        (주문조회시 필요합니다.)
+      </p>
+    </div>
+  </div>
+  <div class="checkPassword inputSort">
+    <div><strong>주문조회 비밀번호 확인</strong></div>
+    <div>
+      <input
+        id="checkPassword"
+        type="text"
+        placeholder="비밀번호를 입력해 주세요"
+        required
+      />
+    </div>
+  </div>`;
+}
+
+function productTemplate(img, title, price, count) {
   return `<li class="product">
     <div>
       <img src=${img} />
@@ -24,11 +64,29 @@ function template(img, title, price, count) {
   </li>`;
 }
 
+function submit(a) {
+  a.preventDefault();
+  if (userCheck) {
+  } else {
+    if (checkPassword.value === password.value) {
+      alert("성공");
+    } else {
+      alert("주문조회 비밀번호가 같은지 확인해주세요");
+    }
+  }
+}
+
+let b = {
+  email: "tutor-sw2@elicer.com",
+  fullName: "tutor",
+  phoneNumber: "010-0000-0000",
+};
+b = JSON.stringify(b);
 if (baskets.length > 0) {
   baskets.forEach((basket) => {
     li.insertAdjacentHTML(
       "beforeend",
-      template(basket.imgaes, basket.title, basket.price, basket.count)
+      productTemplate(basket.imgaes, basket.title, basket.price, basket.count)
     );
     totalPriceValue += basket.price * basket.count;
   });
@@ -41,4 +99,33 @@ if (totalPriceValue >= deliveryMin) {
 delivery.innerHTML = `${deliveryPrice}원`;
 totalPrice.innerHTML = `${totalPriceValue + deliveryPrice}`;
 
-console.log(totalPriceValue);
+async function fakefetch(url) {
+  // 가짜 백엔드 호출
+  return new Promise((resolve, reject) => {
+    return resolve(b);
+  });
+}
+function delay(ms) {
+  // 카테고리 로딩 실험용
+  return new Promise((reslove) => setTimeout(reslove, ms));
+}
+
+const a = async () => {
+  await delay(1000);
+  const json = await fakefetch("url");
+  const data = JSON.parse(json);
+  body.classList.remove("hidden");
+  orderer.value = data.fullName;
+  email.value = data.email;
+  phoneNumber.value = data.phoneNumber;
+};
+
+if (userCheck) {
+  a();
+} else {
+  body.classList.remove("hidden");
+  ordererInfoContents.insertAdjacentHTML("beforeend", passwordHtml());
+  password = document.querySelector("#password");
+  checkPassword = document.querySelector("#checkPassword");
+}
+Info.addEventListener("submit", submit);
