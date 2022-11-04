35972f47518fd371a7db3d4e23d9f15bb4b7b076
borgainkinq <borgainkinq@gmail.com>
2022-11-04 17:23:46 +0900

    fix: db, router, service index file

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export * from './user-service';
 export * from './category-service';
 export * from './product-service';
-=======
-export * from "./user-service";
-export * from "./category-service";
-export * from "./product-service";
-export * from "./order-service";
->>>>>>> hong-feature-order-api
+export * from './order-service';
