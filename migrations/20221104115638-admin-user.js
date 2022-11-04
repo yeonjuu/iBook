88bf0631f100a4fddd02c5354f0696fa913b267f
Seongil lee <modolee.si@gmail.com>
2022-11-04 21:00:33 +0900

    feat: add migration

---
+++ migrations/20221104115638-admin-user.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up(db, client) {
+    return db.collection('users').insertMany([
+      {
+        email: 'abcdef@google.com',
+        fullName: '홍길동',
+      },
+    ]);
+    // TODO write your migration here. Return a Promise (and/or use async & await).
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // return db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+  },
+
+  down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // return db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  },
+};
