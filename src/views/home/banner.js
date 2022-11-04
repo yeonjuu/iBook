232d39f11c40b2bdaa0f6928dfcfd3b6a73d14ac
minkyung <alsrudsla1124@gmail.com>
2022-11-04 15:18:37 +0900

    회원가입 페이지 및 메인 배너 하단 부분 수정

--- src/views/home/banner.js
+++ src/views/home/banner.js
@@ -1,65 +1,65 @@
 // import { application } from "express";
 // //api.js 파일 import
-import * as Api from "../api";
+// import * as Api from "../api";
 
-const fakeData = [
-  //postman으로 생성하고 get하면 이런식의 데이터가 보인다
-  {
-    _id: "6363f42bccf95f9f6fe1d3d9",
-    title: "트렌드코리아2023",
-    author: "서울대",
-    price: 18000,
-    publisher: "출판사",
-    images: ["ur1", "url2"],
-    description: "상세설명",
-    rate: 0,
-    category: "인문",
-    createdAt: "2022-11-03T17:02:35.211Z",
-    updatedAt: "2022-11-03T17:02:35.211Z",
-    __v: 0,
-  },
-  {
-    title: "하얼빈",
-    image:
-      "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788954699914.jpg",
-  },
-  {
-    title: "트렌드 코리아",
-    image:
-      "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788959897094.jpg",
-  },
-  {
-    title: "불편한 편의점2",
-    image:
-      "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791161571188.jpg",
-  },
-  {
-    title: "역행자",
-    image:
-      "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788901260716.jpg",
-  },
-];
+// const fakeData = [
+//   //postman으로 생성하고 get하면 이런식의 데이터가 보인다
+//   {
+//     _id: "6363f42bccf95f9f6fe1d3d9",
+//     title: "트렌드코리아2023",
+//     author: "서울대",
+//     price: 18000,
+//     publisher: "출판사",
+//     images: ["ur1", "url2"],
+//     description: "상세설명",
+//     rate: 0,
+//     category: "인문",
+//     createdAt: "2022-11-03T17:02:35.211Z",
+//     updatedAt: "2022-11-03T17:02:35.211Z",
+//     __v: 0,
+//   },
+//   {
+//     title: "하얼빈",
+//     image:
+//       "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788954699914.jpg",
+//   },
+//   {
+//     title: "트렌드 코리아",
+//     image:
+//       "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788959897094.jpg",
+//   },
+//   {
+//     title: "불편한 편의점2",
+//     image:
+//       "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791161571188.jpg",
+//   },
+//   {
+//     title: "역행자",
+//     image:
+//       "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788901260716.jpg",
+//   },
+// ];
 
-const firstRow = document.querySelector("#firstRow");
-const secondRow = document.querySelector("#secondRow");
-const thirdRow = document.querySelector("#thirdRow");
+// const firstRow = document.querySelector("#firstRow");
+// const secondRow = document.querySelector("#secondRow");
+// const thirdRow = document.querySelector("#thirdRow");
 
-function loadHandler() {
-  const bookImage = fakeData.map((e) => e.image);
-  const bookTitle = fakeData.map((e) => e.title);
+// function loadHandler() {
+//   const bookImage = fakeData.map((e) => e.image);
+//   const bookTitle = fakeData.map((e) => e.title);
 
-  for (let i = 0; i < 5; i++) {
-    const booktags = `<div><a href="/produt">
-        <img src="${bookImage[i]}"/>
-        <span>${bookTitle[i]}</span>
-        </a></div>`;
-    firstRow.insertAdjacentHTML("beforeend", booktags);
-    secondRow.insertAdjacentHTML("beforeend", booktags);
-    thirdRow.insertAdjacentHTML("beforeend", booktags);
-  }
-}
+//   for (let i = 0; i < 5; i++) {
+//     const booktags = `<div><a href="/produt">
+//         <img src="${bookImage[i]}"/>
+//         <span>${bookTitle[i]}</span>
+//         </a></div>`;
+//     firstRow.insertAdjacentHTML("beforeend", booktags);
+//     secondRow.insertAdjacentHTML("beforeend", booktags);
+//     thirdRow.insertAdjacentHTML("beforeend", booktags);
+//   }
+// }
 
-document.addEventListener("DOMContentLoaded", loadHandler);
+// document.addEventListener("DOMContentLoaded", loadHandler);
 
 //임시코드로 firstRow의 div로 잡음
 // const productDiv = document.querySelector("#firstRow > div");
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -5,64 +5,35 @@
 import * as Api from "/api.js";
 import { randomId } from "/useful-functions.js";
 
+
+const firstRow = document.querySelector("#firstRow");
+const secondRow = document.querySelector("#secondRow");
+const thirdRow = document.querySelector("#thirdRow");
+
+
 getProductData();
 
 async function getProductData() {
   const product = await Api.get("api/products");
 
-  console.log(product); 
-}
+  //console.log(product); 
 
-// 요소(element), input 혹은 상수
-// const landingDiv = document.querySelector("#landingDiv");
-// const greetingDiv = document.querySelector("#greetingDiv");
+  const bookImage = await product.map((e) => e.images[0]);
+  const bookTitle = product.map((e) => e.title);
+  const bookId = product.map(e => e._id);
 
-// addAllElements();
-// addAllEvents();
+  console.log(bookImage);
+  console.log(bookTitle);
+  console.log(bookId);
 
-// // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-// async function addAllElements() {
-//   insertTextToLanding();
-//   insertTextToGreeting();
-// }
+  for (let i = 0; i < 5; i++) {
+    const booktags = `<div><a href="/products/${bookId[i]}">
+        <img src=${bookImage[i]}/>
+        <span>${bookTitle[i]}</span>
+        </a></div>`;
+    firstRow.insertAdjacentHTML("beforeend", booktags);
+    secondRow.insertAdjacentHTML("beforeend", booktags);
+    thirdRow.insertAdjacentHTML("beforeend", booktags);
+  };
 
-// // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-// function addAllEvents() {
-//   landingDiv.addEventListener("click", alertLandingText);
-//   greetingDiv.addEventListener("click", alertGreetingText);
-// }
-
-// function insertTextToLanding() {
-//   landingDiv.insertAdjacentHTML(
-//     "beforeend",
-//     `
-//       <h2>n팀 쇼핑몰의 랜딩 페이지입니다. 자바스크립트 파일에서 삽입되었습니다.</h2>
-//     `
-//   );
-// }
-
-// function insertTextToGreeting() {
-//   greetingDiv.insertAdjacentHTML(
-//     "beforeend",
-//     `
-//       <h1>반갑습니다! 자바스크립트 파일에서 삽입되었습니다.</h1>
-//     `
-//   );
-// }
-
-// function alertLandingText() {
-//   alert("n팀 쇼핑몰입니다. 안녕하세요.");
-// }
-
-// function alertGreetingText() {
-//   alert("n팀 쇼핑몰에 오신 것을 환영합니다");
-// }
-
-// async function getDataFromApi() {
-//   // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
-//   const data = await Api.get("/api/user/data");
-//   const random = randomId();
-
-//   console.log({ data });
-//   console.log({ random });
-// }
+};
