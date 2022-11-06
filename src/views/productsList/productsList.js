d26a887b7920383311f4ac366a8137d64a116a4b
minkyung <alsrudsla1124@gmail.com>
2022-11-06 15:45:48 +0900

    카테고리 메뉴 진입시 카테고리에 맞게 정보 노출 처리

--- src/views/productsList/productsList.js
+++ src/views/productsList/productsList.js
@@ -1,28 +1,53 @@
 import * as Api from '/api.js';
 
 const fictionList = document.querySelector(".fictionList");
+const categoryName = document.querySelector(".categoryName");
 
-viewCategory();
+const url = window.location.href;
+const categoryId = url.split("/")[4];
 
-async function viewCategory() {
 
+//상단 카테고리명 추가
+showCategoryName();
+
+async function showCategoryName() {
+  const category = await Api.get("http://localhost:5000/api/categories");
+  console.log(category);
+  const currentCategory = category.filter(e => e._id === categoryId);
+  const currentCategoryName = currentCategory[0].name;
+
+  categoryName.innerText = currentCategoryName;
+
+}
+
+
+showProductsList();
+
+async function showProductsList() {
+
+    // const url = window.location.pathname;
+    // const categoryId = url.split("/")[2];
+    
     const totalBooks = await Api.get("http://localhost:5000/api/products");
-    console.log(totalBooks);
+    //console.log(totalBooks);
 
-    //소설 대신 클릭한 카테고리 id랑 비교하는 방식으로 변경 필요함
-    const scienceFiction = totalBooks.filter( (e) => e.category.name === "소설" );
-    console.log(scienceFiction);
+    //클릭한 카테고리 id 비교
+    const productsList = totalBooks.filter( (e) => e.category._id === `${categoryId}` );
+    //console.log(productsList);
 
+    if(!productsList) {
+        const nothingToShow = `<li>표시할 책 내용이 없습니다</li>`;
+        fictionList.insertAdjacentHTML('beforeend', toShow);
+    };
 
-    for (let i=0; i < scienceFiction.length; i++) {
-
+    for (let i=0; i < productsList.length; i++) {
         const toShow = `
-        <div><img src="${scienceFiction[i].images}"/></div>
-        <li>책이름 : ${scienceFiction[i].title}</li>
-        <li>저자 :${scienceFiction[i].author}</li>
-        <li>출판사 : ${scienceFiction[i].publisher}</li>
-        <li>상세정보 : ${scienceFiction[i].description}</li>
-        <li>금액 : ${scienceFiction[i].price}원</li>
+        <div><img src="${productsList[i].images}"/></div>
+        <li>책이름 : ${productsList[i].title}</li>
+        <li>저자 :${productsList[i].author}</li>
+        <li>출판사 : ${productsList[i].publisher}</li>
+        <li>상세정보 : ${productsList[i].description}</li>
+        <li>금액 : ${productsList[i].price}원</li>
         </div>
         `;
 
