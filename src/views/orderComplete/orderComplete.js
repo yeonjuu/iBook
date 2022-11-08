b828b683722b0dbedf5054b800709a93cbd020ae
odhdhfjf <odhdhfjf@gmail.com>
2022-11-08 17:40:31 +0900

    주문구현완료

--- src/views/orderComplete/orderComplete.js
+++ src/views/orderComplete/orderComplete.js
@@ -4,5 +4,10 @@ const goOderInfoBtn = document.querySelector('.goOrderInfo');
 function goHome() {
   location.replace('/');
 }
-
+function goOderInfo() {
+  location.replace('/changeOrder');
+}
+localStorage.removeItem('carts');
+localStorage.removeItem('cart');
 goHomeBtn.addEventListener('click', goHome);
+goOderInfoBtn.addEventListener('click', goOderInfo);
