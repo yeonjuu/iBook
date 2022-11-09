3cce3ae153681124bd9088692535e01b82fe5931
LeeJiHyeon <rachel2148072@gmail.com>
2022-11-09 12:23:23 +0900

    fix: 유저 api 코드 수정

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -113,6 +113,11 @@ userRouter.put('/:userId', loginRequired, async function (req, res, next) {
     // body data 로부터 업데이트할 사용자 정보를 추출함.
     const { fullName, password, address, phoneNumber, currentPassword } =
       req.body;
+    // currentPassword 없을 시, 진행 불가
+    if (!currentPassword) {
+      throw new Error('정보를 변경하려면, 현재의 비밀번호가 필요합니다.');
+    }
+
     const userInfoRequired = { userId, currentPassword };
 
     // 위 데이터가 undefined가 아니라면, 즉, 프론트에서 업데이트를 위해
